59e2016e2b79a25782743789935425c46b0a1b9c
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-04-17 14:05:19 +0300

    Added a 'problematic' fileviewer, but it works.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import DownloadMethods from './download-file/DownloadMethods';
-
-
-
-
+import ViewFile from './view-file/ViewFile';
 
 function App() {
+  const [toggleFileViewer, setToggleFileViewer] = useState(false);
+
   return (
     <div className='App'>
-      <button onClick={DownloadMethods.downloadFileWithJsFileDownload}>Download</button>
+      <button onClick={DownloadMethods.downloadFileWithJsFileDownload}>
+        Download
+      </button>
       <br />
-      <button onClick={DownloadMethods.downloadWithLinkInDom}>Download with link in dom</button>
+      <button onClick={DownloadMethods.downloadWithLinkInDom}>
+        Download with link in dom
+      </button>
       <br />
-      <button onClick={DownloadMethods.downloadWithLinkInDom2}>Download with link in dom 2</button>
+      <button onClick={DownloadMethods.downloadWithLinkInDom2}>
+        Download with link in dom 2
+      </button>
+      <br />
+      <br />
+      <br />
+      <br />
+
+      
+      <button
+        onClick={() => {
+          setToggleFileViewer(true);
+        }}
+      >
+        Preview File
+      </button>
+
+      <ViewFile
+       open={toggleFileViewer}
+       onDialogClose={() => {
+         setToggleFileViewer(false);
+       }}
+       />
+
+      
     </div>
   );
 }
---
+++ src/view-file/ViewFile.js
@@ -0,0 +1,71 @@
+// library used: https://www.npmjs.com/package/react-file-viewer
+import React from 'react';
+import FileViewer from 'react-file-viewer';
+import axios from 'axios';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from '@material-ui/core';
+
+import CancelIcon from '@material-ui/icons/Cancel';
+
+const onError = (e) => {
+  console.log(e, 'error in file-viewer');
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  cancelButton: {
+    margin: theme.spacing(1),
+    color: 'aliceblue',
+    backgroundColor: 'red',
+    '&:hover': {
+      backgroundColor: theme.palette.error.dark,
+    },
+  },
+}));
+
+const file = 'http://localhost:5000/api/employees/201/companydocuments/2/file';
+const type = 'pdf';
+
+const ViewFile = ({ open, onDialogClose }) => {
+  const classes = useStyles();
+
+  return (
+    <Dialog
+      open={open}
+      onClose={onDialogClose}
+      fullScreen
+    >
+      <DialogTitle id='form-dialog-title'>Preview Document</DialogTitle>
+      <DialogContent>
+        <FileViewer
+          fileType={type}
+          filePath={file}
+          // errorComponent={CustomErrorComponent}
+          onError={onError}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button
+          variant='contained'
+          className={classes.cancelButton}
+          startIcon={<CancelIcon />}
+          onClick={onDialogClose}
+        >
+          Cancel
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ViewFile;
